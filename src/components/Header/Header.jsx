b317dc736c9832a94204220165fdae81ffb0e4e0
Aidarbek Umurzakov <aidarali.prod@gmail.com>
2022-05-01 17:40:26 +0600

    main block is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,29 +2,30 @@ import logoImage from '../../assets/img/levi.svg'
 import './Header.css'
 export const Header = () =>{
     return (
-        <div className='header'>
-            <div className="menu">
-                <li>
-                    <a href="/">SHOP</a>
-                </li>
-                <li>
-                    <a href="/">CUSTOM</a>
-                </li>
-                <li>
-                    <a href="/">DISCOVER</a>
-                </li>
-                <li>
-                    <a href="/">SECONDHAND</a>
-                </li>
-                
-            </div>
-            <div className="logo">
-                <img src={logoImage} alt="" />
-            </div>
-            <div className="search">
-                <input className='searching' type="text" placeholder='What are you looking for?' />
+        <div className="main-head">
+            <div className='header'>
+                <div className="menu">
+                    <li>
+                        <a href="/">SHOP</a>
+                    </li>
+                    <li>
+                        <a href="/">CUSTOM</a>
+                    </li>
+                    <li>
+                        <a href="/">DISCOVER</a>
+                    </li>
+                    <li>
+                        <a href="/">SECONDHAND</a>
+                    </li>
+                    
+                </div>
+                <div className="logo">
+                    <img src={logoImage} alt="" />
+                </div>
+                <div className="search">
+                    <input className='searching' type="text" placeholder='What are you looking for?' />
+                </div>
             </div>
         </div>
-    
     )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,46 @@
-
 import './Main.css'
-export const Header = () =>{
+export const Main = () =>{
     return (
-        <div>
-
-        </div>
+        <section className='main-section'>
+            <div className="first">
+                <h4>
+                    Mystery Sale
+                </h4>
+                
+            </div>
+            <div className="second">
+                <h3>
+                    $75 OFF $200+
+                </h3>
+            </div>
+            <div className="paragraph">
+                <p>
+                You choose your ending in this mystery story. What will your savings be? Get $15 off $75+, $25 off $100+, $50 off $150+ or $75 off $200+.
+                </p>
+            </div>
+            <div className="buttons">
+                <div className="buttons-inside">
+                    <button>
+                        SHOP MEN
+                    </button>
+                    <button>
+                        SHOP WOMEN
+                    </button>
+                    <button>
+                        SHOP KIDS
+                    </button>
+                    <button>
+                        SHOP SALE
+                    </button>
+                </div>
+            </div>
+            <div className="paragraph-2">
+                <p>
+                Online only. Auto-applied at checkout.
+                </p>
+            </div>
+        </section>
+        
     
     )
 }
