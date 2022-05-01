92b6c1ca81d2a155d7dbe516a8d2e5071cb63f11
Aidarbek Umurzakov <aidarali.prod@gmail.com>
2022-05-01 16:50:12 +0600

    header is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,30 @@
-import logoImage from '../../assets/img/logo.svg'
+import logoImage from '../../assets/img/levi.svg'
 import './Header.css'
 export const Header = () =>{
     return (
         <div className='header'>
+            <div className="menu">
+                <li>
+                    <a href="/">SHOP</a>
+                </li>
+                <li>
+                    <a href="/">CUSTOM</a>
+                </li>
+                <li>
+                    <a href="/">DISCOVER</a>
+                </li>
+                <li>
+                    <a href="/">SECONDHAND</a>
+                </li>
+                
+            </div>
             <div className="logo">
                 <img src={logoImage} alt="" />
             </div>
-            <div className="menu">
-            <a href="/">About</a>
-            <a href="/">Products</a>
-            <a href="/">Pricing</a>
-            </div>
-            <div className="logg">
-                <button>Log In</button>
-                <button>Sign Up</button>
+            <div className="search">
+                <input className='searching' type="text" placeholder='What are you looking for?' />
             </div>
         </div>
+    
     )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+
+import './Main.css'
+export const Header = () =>{
+    return (
+        <div>
+
+        </div>
+    
+    )
+}
