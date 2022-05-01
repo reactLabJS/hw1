dc70da81f428c313b76ea722a149864f4ab0a229
Aidarbek Umurzakov <aidarali.prod@gmail.com>
2022-05-01 18:39:34 +0600

    final

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,46 +1,84 @@
 import './Main.css'
+import photo from '../../assets/img/collina.jpeg'
 export const Main = () =>{
     return (
-        <section className='main-section'>
-            <div className="first">
-                <h4>
-                    Mystery Sale
-                </h4>
-                
-            </div>
-            <div className="second">
-                <h3>
-                    $75 OFF $200+
-                </h3>
-            </div>
-            <div className="paragraph">
-                <p>
-                You choose your ending in this mystery story. What will your savings be? Get $15 off $75+, $25 off $100+, $50 off $150+ or $75 off $200+.
-                </p>
-            </div>
-            <div className="buttons">
-                <div className="buttons-inside">
-                    <button>
-                        SHOP MEN
-                    </button>
-                    <button>
-                        SHOP WOMEN
-                    </button>
-                    <button>
-                        SHOP KIDS
-                    </button>
-                    <button>
-                        SHOP SALE
-                    </button>
+        <div>
+            <section className='main-section'>
+                <div className="first">
+                    <h4>
+                        Mystery Sale
+                    </h4>
+                    
                 </div>
-            </div>
-            <div className="paragraph-2">
-                <p>
-                Online only. Auto-applied at checkout.
-                </p>
-            </div>
-        </section>
-        
-    
+                <div className="second">
+                    <h3>
+                        $75 OFF $200+
+                    </h3>
+                </div>
+                <div className="paragraph">
+                    <p>
+                    You choose your ending in this mystery story. What will your savings be? Get $15 off $75+, $25 off $100+, $50 off $150+ or $75 off $200+.
+                    </p>
+                </div>
+                <div className="buttons">
+                    <div className="buttons-inside">
+                        <button>
+                            SHOP MEN
+                        </button>
+                        <button>
+                            SHOP WOMEN
+                        </button>
+                        <button>
+                            SHOP KIDS
+                        </button>
+                        <button>
+                            SHOP SALE
+                        </button>
+                    </div>
+                </div>
+                <div className="paragraph-2">
+                    <p>
+                    Online only. Auto-applied at checkout.
+                    </p>
+                </div>
+            </section>
+            <section className='block'>
+                <div className="pic-block">
+                    <div className="img-inside">
+                        <img src={photo} alt="" />
+                    </div>
+                </div>
+                <div className="text-block">
+                    <div className="text-block-inside">
+                        <div className="text-blockkk">
+                            <div className="text-one">
+                                <h4>
+                                    The Drop
+                                </h4>
+                            </div>
+                            <div className="text-two">
+                                <h3>
+                                    LEVI'S® X COLLINA STRADA
+                                </h3>
+                            </div>
+                            <div className="text-three">
+                                <p>
+                                For Spring, we teamed up with Hillary Taymour and her brand and social platform Collina Strada on a bright, garden-inspired denim capsule collection.
+                                </p>
+                            </div>
+                            <div className="text-four">
+                                <span>
+                                    <a href="/">
+                                        EXPLORE THE COLLECTION
+                                    </a>
+                                </span>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
     )
 }
